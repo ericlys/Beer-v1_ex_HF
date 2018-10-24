a9c52de569b6dd76bc995e47ca5a8cf99398b3bd
Ériclys <ericlysm@gmail.com>
2018-10-23 21:43:37 -0300

    utilisando jsp

--- src/com/example/web/BeerSelect.java
+++ src/com/example/web/BeerSelect.java
@@ -10,20 +10,18 @@ public class BeerSelect extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
 
+        String c = request.getParameter("color");
+        BeerExpert be = new BeerExpert();
+        List result = be.getBrands(c);
 
-         response.setContentType("text/html");
-         PrintWriter out = response.getWriter();
-         out.println("Beer Selection Advice<br>");
-         String c = request.getParameter("color");
-        /* out.println("<br>Got beer color " + c);*/
+        /*response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Beer Selection Advice<br>");*/
 
+        request.setAttribute("styles", result);
 
-         BeerExpert be = new BeerExpert();
-         List result = be.getBrands(c);
-
-         Iterator it = result.iterator();
-         while (it.hasNext()){
-             out.print("<br>try: " +it.next());
-         }
+        RequestDispatcher view =
+                request.getRequestDispatcher("result.jsp");
+        view.forward(request, response);
     }
 }
